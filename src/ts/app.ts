fc8de68e22af1874c9373e4bab26d38ab0e41504
Yellow May <kwerep09@yahoo.com>
2021-07-19 22:22:48 +0100

    📦 NEW: App Js initial setup

---
+++ src/ts/app.ts
@@ -0,0 +1,14 @@
+// expense section
+const balanceEl = document.getElementById("balance");
+const balanceTypeEl = document.getElementById("balanceType");
+const profitEl = document.getElementById("profit");
+const lossEl = document.getElementById("loss");
+
+// transactions section
+const transactionsListEl = document.getElementById("transactionsList");
+
+// form section
+const formEl = document.querySelector("form");
+const descInp = document.getElementById("desc");
+const typesSel = document.getElementById("types");
+const dateInp = document.getElementById("date");
